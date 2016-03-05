3a10dcf953528d31ce040395afb0c402c5021f14
dayhkr <dayhkr@gmail.com>
2016-03-04 20:02:06 -0700

    Initial Commit

---
+++ issuecommand.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+from rustadmin import RustAdmin
+
+cs = RustAdmin(url='192.168.1.137:5678', passwd='swUrUBr2kA')
+mess = cs.sndcommand(msg='global.teleportany')
+
+print mess['Message']
+#inventory.giveto dayhkr wood 100
+#server.writecfg
+#weather.clouds
+#weather.fog
+#weather.wind
+#weather.rain
+#global.playerlist
+#global.users
+#global.say
+#global.ownerid
+#global.moderatorid
+#global.status
+#global.kick
+#global.listid
---
+++ rustadmin.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+import websocket
+import json
+
+class RustAdmin(object):
+    def __init__(self, url, passwd):
+        self.ws=websocket.create_connection("ws://" + url + '/' + passwd)
+    def sndcommand(self, msg):
+        print("Sending Message")
+        packet = {
+		    		"Identifier": "1",
+			    	"Message": msg,
+				    "Name": "WebRcon"
+			    }
+        self.ws.send(json.dumps(packet))
+        print("Sent")
+        print("Receiving...")
+        result = json.loads(self.ws.recv())
+        self.ws.close()
+        return result
+
+
