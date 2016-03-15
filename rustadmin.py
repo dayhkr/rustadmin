f0cc34453f73f0150deeb22e2e7d4d2aa0c2768f
eploughe <eploughe@godaddy.com>
2016-03-14 20:40:26 -0700

    Cleaned up code now Pep8

--- rustadmin.py
+++ rustadmin.py
@@ -2,16 +2,18 @@
 import websocket
 import json
 
+
 class RustAdmin(object):
     def __init__(self, url, passwd):
-        self.ws=websocket.create_connection("ws://" + url + '/' + passwd)
+        self.ws = websocket.create_connection("ws://" + url + '/' + passwd)
+
     def sndcommand(self, msg):
         print("Sending Message")
         packet = {
-		    		"Identifier": "1",
-			    	"Message": msg,
-				    "Name": "WebRcon"
-			    }
+                    "Identifier": "1",
+                    "Message": msg,
+                    "Name": "WebRcon"
+                 }
         self.ws.send(json.dumps(packet))
         print("Sent")
         print("Receiving...")
